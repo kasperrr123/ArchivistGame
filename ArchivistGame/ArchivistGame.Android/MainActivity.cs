6c3a9182eb1682ea4ca9966626991101da661540
Kasper Frank Jørgensen <kasp7244@easv365.dk>
2018-05-24 23:29:59 +0200

    Changes. Trying to javabind to get chromecast to work.

--- ArchivistGame/ArchivistGame.Android/MainActivity.cs
+++ ArchivistGame/ArchivistGame.Android/MainActivity.cs
@@ -3,11 +3,14 @@ using Android.App;
 using Android.Content.PM;
 using Android.OS;
 
+
+
 namespace ArchivistGame.Droid
 {
     [Activity(Label = "ArchivistGame", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        
         protected override void OnCreate(Bundle bundle)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
